be3bb54edd6b3cd85d2286fbc4d54b07de287924
Tibluka <cprpc@hotmail.com>
2023-02-15 17:15:19 -0300

    .

--- assets/src/pages/Camera.tsx
+++ assets/src/pages/Camera.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text } from 'react-native'
 
+import { Camera as ExpoCamera, CameraType } from 'expo-camera';
+
 const Camera: React.FC = () => {
+    const [hasPermission, setHasPermission] = useState<any>(null);
+    const [type, setType] = useState(CameraType.back);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await ExpoCamera.requestCameraPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
+
+    if (hasPermission === null) {
+        return <View />;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
     return (
-        <View/>
+        <View style={{ flex: 1 }}>
+            <ExpoCamera style={{ flex: 1 }} />
+        </View>
     )
 }
 
