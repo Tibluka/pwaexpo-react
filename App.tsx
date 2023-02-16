2e7e351b20f6ff80f0f0e21241fc8ef21272cdf5
Tibluka <cprpc@hotmail.com>
2023-02-16 16:21:25 -0300

    .

--- App.tsx
+++ App.tsx
@@ -1,6 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Routes from './assets/src/routes';
+import Routes from './src/routes';
 
 export default function App() {
   return (
